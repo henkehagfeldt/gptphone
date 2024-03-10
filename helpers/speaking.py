a0a0a7b0f8378d34bc8d9a0ef1f158d62e0ee321
Henrik Hagfeldt <henrik.hagfeldt@agreat.com>
2024-03-10 17:47:14 +0100

    testing

--- helpers/speaking.py
+++ helpers/speaking.py
@@ -1,13 +1,6 @@
-#import pyttsx3
-
-#engine = pyttsx3.init()
-
-#def say(prompt):
-#    print(f"Saying {prompt}")
-#    engine.say(prompt)
-#    engine.runAndWait()
-
 from gtts import gTTS
+import pygame
+from io import BytesIO
 
 # This module is imported so that we can  
 # play the converted audio 
@@ -15,18 +8,17 @@ import os
 
 def say(prompt):
 
-    # Language in which you want to convert 
-    language = 'en'
+    tts = gTTS(text=prompt, lang="en")
+    
+    # convert to file-like object
+    fp = BytesIO()
+    tts.write_to_fp(fp)
+    fp.seek(0)
 
-    # Passing the text and language to the engine,  
-    # here we have marked slow=False. Which tells  
-    # the module that the converted audio should  
-    # have a high speed 
-    myobj = gTTS(text=prompt, lang=language, slow=False) 
-
-    # Saving the converted audio in a mp3 file named 
-    # welcome  
-    myobj.save("welcome.mp3") 
-
-    # Playing the converted file 
-    os.system("aplay welcome.mp3") 
+    
+    pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load(fp)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        pygame.time.Clock().tick(10)
