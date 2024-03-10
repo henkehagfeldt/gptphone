4764d08bb63f938129575c6536bb8e67e3633366
Henrik Hagfeldt <henrik.hagfeldt@agreat.com>
2024-03-10 18:06:29 +0100

    test

--- helpers/speaking.py
+++ helpers/speaking.py
@@ -1,6 +1,5 @@
 from gtts import gTTS
 from io import BytesIO
-import simpleaudio as sa
 
 # This module is imported so that we can  
 # play the converted audio 
@@ -16,12 +15,3 @@ def say(prompt):
     #fp.seek(0)
 
     subprocess.call(["vlc", "response.mp3"])
-
-    play_audio(fp)
-
-def play_audio(fp):
-    fp.seek(0)
-    audio_data = fp.read()
-    wave_obj = sa.WaveObject(audio_data, num_channels=2, bytes_per_sample=2, sample_rate=44100)
-    play_obj = wave_obj.play()
-    play_obj.wait_done()
