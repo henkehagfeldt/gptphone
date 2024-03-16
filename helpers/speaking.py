36c11b11d39f2017b1e926c91108b9acb29d1ae6
Henrik Hagfeldt <henrik.hagfeldt@agreat.com>
2024-03-16 23:59:59 +0100

    wait for sound

--- helpers/speaking.py
+++ helpers/speaking.py
@@ -1,5 +1,6 @@
 from gtts import gTTS
 from pygame import mixer  # Load the popular external library
+import time
 
 def say(prompt):
     tts = gTTS(text=prompt, lang="en")
@@ -8,3 +9,6 @@ def say(prompt):
     mixer.init()
     mixer.music.load("response.mp3")
     mixer.music.play()
+
+    while mixer.music.get_busy():  # wait for music to finish playing
+        time.sleep(0.1)
